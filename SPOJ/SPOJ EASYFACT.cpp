dc7441c88a5085cbc62d6667d2d36e37386eb1f9
Ishmeet Singh Saggu <57167867+Psychik-N@users.noreply.github.com>
2020-08-02 20:35:43 +0530

    Update SPOJ EASYFACT.cpp

--- SPOJ/SPOJ EASYFACT.cpp
+++ SPOJ/SPOJ EASYFACT.cpp
@@ -1,4 +1,7 @@
 /*
+	Problem Link :
+	* https://www.spoj.com/problems/EASYFACT/
+	
 	Idea :
 	* let N! = (a+1) + (a+2) ... (a+l). Applying AP formula 2*N! = (l*(2*a+l-1)), where l and (2*a+l-1) have different parity. So number of these pair
 	- combination satisfying these properties will be equal to number of odd divisors of N!.
